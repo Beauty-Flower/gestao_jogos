b08f6bf58648f237ef3d7e4514973119581d37d6
Florbela <flor20012005@gmail.com>
2021-11-14 23:07:34 -0300

    getters and setters

--- src/br/senai/sp/jandira/model/Fabricante.java
+++ src/br/senai/sp/jandira/model/Fabricante.java
@@ -6,4 +6,20 @@ public class Fabricante {
 
 	private String nome;
 	private LocalDate dataFundação;
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setDataFundação(LocalDate dataFundação) {
+		this.dataFundação = dataFundação;
+	}
+	
+	public LocalDate getDataFundação() {
+		return dataFundação;
+	}
 }
