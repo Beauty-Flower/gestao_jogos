f9e4de8da8b485f0999cbaca2abe3df64eddbce0
Florbela <flor20012005@gmail.com>
2021-11-18 10:35:45 -0300

    Criando ENUM

--- src/br/senai/sp/jandira/model/Console.java
+++ src/br/senai/sp/jandira/model/Console.java
@@ -1,5 +1,18 @@
 package br.senai.sp.jandira.model;
 
 public enum Console {
-
+	
+	COLORTVGAME("Color TV Game"),
+	NINTENDO64("Nintendo 64"),
+	NINTENDOSWITCH("Nintendo Switch");
+	
+	private String descricao;
+	
+	private Console(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
 }
